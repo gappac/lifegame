149061068f7992cd48703b5ff7e6a47b55f67be6
gappac <rikubump3370@gmail.com>
2024-08-10 16:38:45 +0900

    アルファ版

--- lifegame.py
+++ lifegame.py
@@ -1,2 +1,67 @@
-print("lifegame")
+from random import randint
+from tkinter import *
+from tkinter import ttk
 
+# 変数・定数の定義 --- (*1)
+COLS, ROWS = [30, 20] # ステージのサイズを定義
+CW = 20 # セルの描画サイズ
+data = [] # ステージデータ
+for y in range(0, ROWS): # ステージをランダムに初期化
+    data.append([(randint(0, 9) <= 3) for x in range(0, COLS)])
+
+
+# ライフゲームのルールを実装したもの --- (*2)
+def check(x, y):
+    # 周囲の生存セルを数える
+    cnt = 0
+    tbl = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+    for t in tbl:
+        xx, yy = [x + t[0], y + t[1]]
+        if 0 <= xx < COLS and 0 <= yy < ROWS:
+            if data[yy][xx]: cnt += 1
+    # ルールに沿って次世代の生死を決める
+    if cnt == 3: return True # 誕生
+    if data[y][x]:
+        if 2 <= cnt <= 3: return True # 生存
+        return False # 過疎 or 過密
+    return data[y][x]
+
+# データを次の世代に進める --- (*3)
+def next_turn():
+    global data
+    data2 = []
+    for y in range(0, ROWS):
+        data2.append([check(x, y) for x in range(0, COLS)])
+    data = data2 # データの内容を次の世代へ差し替え
+
+
+
+# 画面を構築 --- (*4)
+root = Tk() # ウィンドウを作成
+root.title("ライフゲーム")
+
+
+root_width = 600
+root_height = 400
+
+cv = Canvas(root, width = root_width, height = root_height) # キャンバスを作成
+cv.pack()
+cv.create_line(0, 0, root_width, 0, width=10)
+
+
+# ステージを描画 --- (*5)
+def draw_stage():
+    cv.delete('all') # 既存の描画内容を破棄
+    for y in range(0, ROWS):
+        for x in range(0, COLS):
+            if not data[y][x]: continue
+            x1, y1 = [x * CW, y * CW]
+            cv.create_rectangle(x1+1, y1+1, x1 + CW-1, y1 + CW-1,
+                fill="red", width=0) # 生きているセルを描画
+# 300ミリ秒ごとに世代を進める --- (*6)
+def game_loop():
+    next_turn() # 世代を進める
+    draw_stage() # ステージを描画
+    root.after(1000, game_loop) # 指定時間後に再度描画
+game_loop() # ゲームループを実行
+root.mainloop() # イベントループ
